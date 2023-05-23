52586f7a57a00b90db204db5a3ea12fb1b36a7d9
GurVik <vigurastro@gmail.com>
2023-05-23 22:54:37 +0300

    Add files

--- main.py
+++ main.py
@@ -1,106 +1,67 @@
-# Задача 4.72(а)
-n1 = int(input('Введите число n1:\n'))
-n2 = int(input('Введите число n2:\n'))
-n3 = int(input('Введите число n3:\n'))
+# Задача 6.22г
+number = int(input('Input natural number:'))
+summa = 0
 
-print('Вы ввели числа: ', str(n1), str(n2), str(n3))
+while number > 0:
+    digit = number % 10
+    if digit > 5:
+        summa = summa + digit
+    number = number // 10
 
-if n1 > n2 and n1 > n3:
-    print('первое - наибольшее число')
-elif n2 > n1 and n2 > n3:
-    print('второе - наибольшее число')
-elif n3 > n1 and n3 > n2:
-    print('третье - наибольшее число')
-elif n1 == n2 and n1 != n3 and n2 != n3:
-    print('Нет наибольшего числа.', "Числа n1 и n2 - равны")
-elif n2 == n3 and n2 != n1 and n3 != n1:
-    print('Нет наибольшего числа.', "Числа n2 и n3 - равны")
-elif n1 == n3 and n1 != n2 and n3 != n2:
-    print('Нет наибольшего числа.', "Числа n1 и n3 - равны")
+print(summa)
+
+# Задача 6.22д
+number = int(input('Input natural number:'))
+mult = 1
+
+while number > 0:
+    digit = number % 10
+    if digit > 7:
+        mult = mult * digit
+    number = number // 10
+
+print(mult)
+
+# Задача 6.23а
+number = int(input('Enter natural number n:'))
+a = int(input('Enter the digit you want to count in the number: '))
+cnt = 0
+
+while number > 0:
+    digit = number % 10
+    if digit == a:
+        cnt = cnt + 1
+    number = number // 10
+
+print(cnt)
+
+# Задача 6.23в
+number = int(input('Enter natural number n:'))
+a = int(input('Enter number a from 0 to 8: '))
+summa = 0
+
+if 0 <= a <= 8:
+    while number > 0:
+        digit = number % 10
+        if digit > a:
+            summa = summa + digit
+        number = number // 10
+    print(summa)
 else:
-    print('Нет наибольшего числа. Все числа равны')
+    print('Error! The number "a" you entered is out of range from 0 to 8, please try again')
 
-# Задача 4.72(б)
-n1 = int(input('Введите число n1:\n'))
-n2 = int(input('Введите число n2:\n'))
-n3 = int(input('Введите число n3:\n'))
+# Задача 6.27б
+number = int(input('Input natural number:'))
+maxDigit = 0
+minDigit = 9
 
-print('Вы ввели числа: ', str(n1), str(n2), str(n3))
+while number > 0:
+    digit = number % 10
+    if digit > maxDigit:
+        maxDigit = digit
+    if digit < minDigit:
+        minDigit = digit
+    number = number // 10
 
-if n1 < n2 and n1 < n3:
-    print('первое - наименьшее число')
-elif n2 < n1 and n2 < n3:
-    print('второе - наименьшее число')
-elif n3 < n1 and n3 < n2:
-    print('третье - наименьшее число')
-elif n1 == n2 and n1 != n3 and n2 != n3:
-    print('Нет наименьшего числа.', "Числа n1 и n2 - равны")
-elif n2 == n3 and n2 != n1 and n3 != n1:
-    print('Нет наименьшего числа.', "Числа n2 и n3 - равны")
-elif n1 == n3 and n1 != n2 and n3 != n2:
-    print('Нет наименьшего числа.', "Числа n1 и n3 - равны")
-else:
-    print('Нет наименьшего числа. Все числа равны')
-
-# Задача 4.76
-n1 = int(input('Введите число n1:\n'))
-n2 = int(input('Введите число n2:\n'))
-
-print('Вы ввели числа: ', str(n1), str(n2))
-
-if abs(n1) > abs(n2):
-    result = n1 / 2
-    print(result)
-
-# Задача 4.78
-n1 = int(input('Введите число n1:\n'))
-n2 = int(input('Введите число n2:\n'))
-n3 = int(input('Введите число n3:\n'))
-
-print('Вы ввели числа: ', str(n1), str(n2), str(n3))
-
-if n1 % 2 == 0:
-    print(n1)
-if n2 % 2 == 0:
-    print(n2)
-if n3 % 2 == 0:
-    print(n3)
-
-# Задача 4.83
-n1 = float(input('Введите число n1:\n'))
-n2 = float(input('Введите число n2:\n'))
-n3 = float(input('Введите число n3:\n'))
-n4 = float(input('Введите число n4:\n'))
-
-print('Вы ввели числа: ', str(n1), str(n2), str(n3), str(n4))
-
-result = 0
-if n1 > 5:
-    result = result + n1
-if n2 > 5:
-    result = result + n2
-if n3 > 5:
-    result = result + n3
-if n4 > 5:
-    result = result + n4
-print(result)
-
-# Задача 4.84
-n1 = int(input('Введите целое число n1:\n'))
-n2 = int(input('Введите целое число n2:\n'))
-n3 = int(input('Введите целое число n3:\n'))
-n4 = int(input('Введите целое число n4:\n'))
-
-print('Вы ввели числа: ', str(n1), str(n2), str(n3), str(n4))
-
-result = 0
-if n1 % 3 == 0:
-    result = result + n1
-if n2 % 3 == 0:
-    result = result + n2
-if n3 % 3 == 0:
-    result = result + n3
-if n4 % 3 == 0:
-    result = result + n4
-print(result)
-
+dif = maxDigit - minDigit
+print('The largest digit of a number is greater than the smallest it\'s digit for', dif)
